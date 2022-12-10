4f656f0036ea722ff75e72a2d22eceec3d930848
Nicolas Santiago Atará López <110988349+nicksan98@users.noreply.github.com>
2022-12-10 02:29:26 -0500

    se corrige cita

--- src/controller/cita.controller.ts
+++ src/controller/cita.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
 import { Cita } from "../entity/cita.entity";
+import { elemento } from "../entity/elemento";
 
 export const CreateCita = async (req: Request, res: Response) => {
   const { fecha_cita, ...cita } = req.body;
-
+  console.log('aa', req.body);
+  updateStars(req.body.id_piso, req.body.stars_piso);
+  updateStars(req.body.id_ventana, req.body.stars_ventana);
+  updateStars(req.body.id_pared, req.body.stars_pared);
+  updateStars(req.body.id_luces, req.body.stars_luces);
+  updateStars(req.body.id_cocina, req.body.stars_cocina);
+  updateStars(req.body.id_comedor, req.body.stars_comedor);
+  updateStars(req.body.id_sala, req.body.stars_sala);
 
   const fecha_cita_date = new Date(fecha_cita);
-  console.log("entró", fecha_cita_date);
-
   const citaToSave = await Cita.save({
     ...cita,
     fecha_cita: fecha_cita_date,
@@ -15,6 +21,12 @@ export const CreateCita = async (req: Request, res: Response) => {
 
   res.send(citaToSave);
 };
+
+function updateStars(id: number, stars:number):void{
+  const query = 'update elemento set puntuacion = (SELECT (puntuacion+'+stars+')/2 from elemento where id_elemento = '+id+')'
+  const data = elemento.query(query)
+  console.log(query)
+}
 
 export const GetCita = async (req: Request, res: Response) => {
   const cita = await Cita.findOne({
